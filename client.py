9647cf688039ed1e41e5c69bebbe048e9c961259
Dan Whitehouse <drw@dim-sum.org>
2018-01-23 22:04:46 +0000

    Adding this shit...

---
+++ client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+from random import randint
+from random import randrange
+
+# Get colour
+
+def getcolour():
+  r = randint(0,255)
+  g = randint(0,255)
+  b = randint(0,255)
+  return[r,g,b]
+
+HOST, PORT ="10.201.0.36", 5000
+
+r,g,b = getcolour()
+
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(data + "\n")
+
+    # Receive data from the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)
