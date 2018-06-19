aa78333e24899b5d2e91c1518f94e4389ede1c09
Dan Whitehouse <drw@dim-sum.org>
2018-06-19 20:33:03 +0100

    Fixed lightshow imports

--- uhl/lightshow.py
+++ uhl/lightshow.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """ lightshow.py """
 
-import uhl.lsc
-import uhl.lsd
+import .lsc
+import .lsd
 
 def lightshow(myred, mygreen, myblue):
     """ lightshow """
 
     # Initialise the Unicorn Hat
 
-    uhl.lsd.initunicornhat()
+    lsd.initunicornhat()
 
-    for _ in range(0, 15):
-        uhl.lsd.pulse(myred, mygreen, myblue)
+    for _ in range(0, 5):
+        lsd.pulse(myred, mygreen, myblue)
 
-    for _ in range(0, 15):
-        myred, mygreen, myblue = uhl.lsc.shiftcolour(myred, mygreen, myblue)
-        uhl.lsd.pulse(myred, mygreen, myblue)
+    for _ in range(0, 10):
+        myred, mygreen, myblue = lsc.shiftcolour(myred, mygreen, myblue)
+        lsd.pulse(myred, mygreen, myblue)
 
 if __name__ == "__main__":
 
--- uhl/lsc.py
+++ uhl/lsc.py
@@ -16,10 +16,31 @@ def getcolour():
 
 def shiftcolour(red, green, blue):
     """ shifts colour """
-    red = (red + 10) % 255
-    green = (green + 10) % 255
-    blue = (blue + 10) % 255
+    red = (red + 1) % 255
+    green = (green + 1) % 255
+    blue = (blue + 1) % 255
+    return[red, green, blue]
+
+def warpcolour(red, green, blue):
+    """ warps colour """
+    warp = [-1, 0, 1]
+    red = (red + random.choice(warp)) % 255
+    green = (green + random.choice(warp)) % 255
+    blue = (blue + random.choice(warp)) % 255
     return[red, green, blue]
 
 if __name__ == "__main__":
     print("I am the colour module.")
+    colour = getcolour()
+    print(format(colour))
+    print(type(colour))
+    shift = [-1, 0, 1]
+    print(format(shift))
+    shift = random.choice(shift)
+    print(format(shift))
+    red = 10
+    green = 10
+    blue = 10
+    colour = warpcolour(red, green, blue)
+    print(format(colour))
+    print(type(colour))
